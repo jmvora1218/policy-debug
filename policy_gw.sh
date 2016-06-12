abe34238a822c1075f71632f444c5d1f9a9ee3df
seiruss <noodlesnose88@gmail.com>
2016-06-12 18:06:32 -0500

    Added MIT license as a flag

--- policy_gw.sh
+++ policy_gw.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-# MIT License
-
-# Copyright (c) 2016 Russell Seifert
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 ###############################################################################
 # HELP SCREEN
 ###############################################################################
@@ -34,22 +12,48 @@ HELP_USAGE="Usage: $0 [OPTIONS]
    -f    enable more kernel debug flags
    -m    disable minimum disk space check. files will be written
            to /var/log/tmp/debug
+   -l    license terms
    -v    version information
 "
 
 HELP_VERSION="
 Gateway Policy Debug Script
-Version 2.8 June 6, 2016
+Version 2.8 June 13, 2016
+"
+
+HELP_LICENSE="
+MIT License
+
+Copyright (c) 2016 Russell Seifert
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the \"Software\"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 "
 
 OPTIND=1
-while getopts ':h-:b-:d-:f-:m-:v-:' HELP_OPTION; do
+while getopts ':h-:b-:d-:f-:m-:l-:v-:' HELP_OPTION; do
     case "$HELP_OPTION" in
         h) echo "$HELP_USAGE" ; exit ;;
         b) DEBUG_BUFFER_ON=1 ;;
         d) set -vx ; exec &> >(tee script_debug.txt) ;;
         f) MORE_DEBUG_FLAGS=1 ;;
         m) SPACE_CHECK_OFF=1 ;;
+        l) echo "$HELP_LICENSE" ; exit ;;
         v) echo "$HELP_VERSION" ; exit ;;
         \?) printf "Invalid option: -%s\n" "$OPTARG" >&2
             echo "$HELP_USAGE" >&2 ; exit 1 ;;
